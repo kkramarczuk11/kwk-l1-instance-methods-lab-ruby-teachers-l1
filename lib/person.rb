122851d41f358ffb01c9cc54a5845018499f8397
kkramarczuk11 <kkramarczuk11@gmail.com>
2018-06-21 00:15:47 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,24 +1,9 @@
 # person.rb
 class Person
-attr_accesor :walk
-  def talk=(talk)
-    @talk = talk
-  end
-
-  def talk
-    @talk
-  end
+attr_accesor :talk, :walk
 
   def talk
     puts "Hello World!"
-  end
-
-  def walk=(walk)
-    @walk = walk
-  end
-
-  def walk
-    @walk
   end
 
   def walk
